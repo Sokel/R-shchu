c836dceef170be74b7a1ff7a3ee54d766c94d493
Sokel <pirojenka99@gmail.com>
2015-07-09 21:08:17 +0500

    Create intro.r

---
+++ intro.r
@@ -0,0 +1,23 @@
+1 : 67 
+
+my_vector1 <- 1 : 67 
+my_vector <-  c(-34, 64 ,-54, 543, 2543, 0 , -65 )
+
+my_vector2
+
+
+my_vector1 + 10
+
+my_vector2[0]
+
+my_vector2 == 0
+
+v1 <- 1 : 50 
+v2 <- 60 : 100
+v3 <- c(v1, v2)
+print(v3)
+print(my_vector[c(2,5,7,9,12,16,20)])
+my_vector + 1;
+print(my_vector)
+my_sum <- sum(my_vector[my_vector > 0])
+print(my_sum)
